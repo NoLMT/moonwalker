4520fa0028edbd8c52512f8036b23d571e610476
Querido <tyquerido@gmail.com>
2016-05-13 14:32:02 -0400

    Add files via upload

--- SpacemanSam.java
+++ SpacemanSam.java
@@ -8,15 +8,25 @@ public class SpacemanSam
 {
   static int x = 50;
   static int y = 815;
+  //Horizontal Velocity
   static int xv = 0;
+  //Vertical Velocity
   static int yv = 0;
+  //Horizontal Acceleration
   static int xa = 0; 
+  //Vertical Acceleration
   static int ya = 0;
+  //Number of Jump Boosts
   static int jumpBoost = 2;
+  //Number of Shink Powers
   static int small = 2;
+  //Number of Speed Boosts
   static int speedBoost = 2;
+  //Time spent small
   static int smallTime = 250;
+  //Test if Jumping
   static boolean isJumping = false;
+  //Test if Small
   static boolean isSmall = false;
   MoonWalker m;
   
@@ -27,11 +37,14 @@ public class SpacemanSam
   
   public void move()
   {
+    //If jumping then accelerate downward
     if(isJumping)
     {
     ya = 1;
     }
+    //Move Horizontally
     x += xv;
+    //Stop SMS from moving to quickly
     if(xv > 2)
     {
       xv -= 2;
@@ -40,6 +53,7 @@ public class SpacemanSam
     {
       xv += 2;
     }
+    //Stop SMS from falling through the floor
     if(y >= 815 && yv > 0)
     {
       ya = 0;
@@ -47,11 +61,13 @@ public class SpacemanSam
       y = 815;
       isJumping = false;
     }
+    //Stop SMS from walking off the left side of the screen
     if(x <= 0 && xv < 0)
     {
       x = 0;
       xv = 0;
     }
+    //Stop SMS from walking off the right side of the screen
     if(x >= 860 && xv > 0)
     {
       x = 860;
@@ -60,10 +76,12 @@ public class SpacemanSam
     xv += xa;
     y += yv;
     yv += ya;
+    //CountDown the smallTime
     if(isSmall)
     {
       smallTime--;
     }
+    //Unshrink SMS when timer runs out
     if(smallTime == 0)
     {
       isSmall = false;
@@ -74,6 +92,7 @@ public class SpacemanSam
   public void paint(Graphics2D g2d)
   {
     g2d.setColor(new Color(0, 255, 0));
+    //Paint based on size
     if(!isSmall)
     {
       g2d.fillRect(x, y, 25, 50);
@@ -86,6 +105,7 @@ public class SpacemanSam
   
   public static void keyReleased(KeyEvent e) 
   {
+    //Set momentum to zero when key is released
     if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT)
     {
       xv = 0;
@@ -95,10 +115,12 @@ public class SpacemanSam
   
   public static void keyPressed(KeyEvent e)
   {
+    //Jump
     if (e.getKeyCode() == KeyEvent.VK_UP)
     {
       if(!isJumping)
       {
+        //Jump based on height
         if(!isSmall)
         {
           yv = -15;
@@ -111,6 +133,7 @@ public class SpacemanSam
         isJumping = true;
       }
     }
+    //SpeedBoost
     if (e.getKeyCode() == KeyEvent.VK_Q)
     {
       if(speedBoost > 0 && xv != 0)
@@ -127,6 +150,7 @@ public class SpacemanSam
         speedBoost--;
       }
     }
+    //Reset level
     if (e.getKeyCode() == KeyEvent.VK_R)
     {
       x = 50;
@@ -142,6 +166,7 @@ public class SpacemanSam
       isJumping = false;
       isSmall = false;
     }
+    //Jump Boost
     if (e.getKeyCode() == KeyEvent.VK_W)
     {
       if(jumpBoost > 0)
@@ -159,6 +184,7 @@ public class SpacemanSam
         jumpBoost--;
       }
     }
+    //Shrink
     if (e.getKeyCode() == KeyEvent.VK_E)
     {
       if(small > 0 && !isSmall)
@@ -167,10 +193,12 @@ public class SpacemanSam
         small--;
       }
     }
+    //Walk Left
     if (e.getKeyCode() == KeyEvent.VK_LEFT)
     {
       xa = -1;
     }
+    //Walk Right
     if (e.getKeyCode() == KeyEvent.VK_RIGHT)
     {
       xa = 1;
