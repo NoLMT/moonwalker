e50b56b051661e3a5f0cbac766c177db1f0ef6d3
mounica06 <mounica_06@hotmail.com>
2016-05-30 13:05:43 -0400

    Add files via upload

--- MoonWalker.java
+++ MoonWalker.java
@@ -9,23 +9,18 @@ public class MoonWalker extends JPanel
   SpacemanSam sam = new SpacemanSam(this);
   Platform[] p = new Platform[186];
   private int s = 0;
-  Coin c1 = new Coin(this, 500, 800, 0);
-  Coin c2 = new Coin(this, 800, 400, 1);
-  Coin c3 = new Coin(this, 100, 300, 2);
-  Button b = new Button(this, 800, 570, 3);
-  static String time;
-  static long startTime = System.currentTimeMillis();
-  static long endTime;
-  
+  Coin c1 = new Coin(this, 480, 360, 0);
+  Coin c2 = new Coin(this, 450, 815, 1);
+  Coin c3 = new Coin(this, 90, 120, 2); 
   public MoonWalker()
   {
     KeyListener listener = new KeyListener()
     {
-      public void keyTyped(KeyEvent e){}  
-      public void keyReleased(KeyEvent e) 
+      public void keyTyped(KeyEvent e){}
+      public void keyReleased(KeyEvent e)
       { sam.keyReleased(e); }
-      public void keyPressed(KeyEvent e) 
-      { sam.keyPressed(e);} 
+      public void keyPressed(KeyEvent e)
+      { sam.keyPressed(e);}
     };
     addKeyListener(listener);
     setFocusable(true);
@@ -62,7 +57,7 @@ public class MoonWalker extends JPanel
       }
     }
     s = 130 ;
-    for(int i = 510; i <= 540; i += 30)
+    for(int i = 480; i <= 510; i += 30)
     {
       for (int l = 510; l <= 660; l += 30)
       {
@@ -89,14 +84,29 @@ public class MoonWalker extends JPanel
       }
     }
     s = 174;
-    for(int i = 330; i <= 360; i += 30)//2
+    for(int i = 330; i <= 360; i += 30)
     {
-      for (int l = 210; l <= 360; l += 30)//6
+      for (int l = 210; l <= 360; l += 30)
       {
         p[s] = new Platform(this, l, i);
         s++;
       }
     }
+     s = 186;
+    for(int i = 300; i <= 360; i += 30)//3
+    {
+      for (int l = 210; l <= 300; l += 30)//4
+      {
+        p[s] = new Platform(this, l, i);
+        s++;
+      }
+    }
+  }
+  public void unCollect()
+  {
+    c1.unCollect();
+    c2.unCollect();
+    c3.unCollect();
   }
   
   public static void main(String[] args) throws InterruptedException
@@ -104,55 +114,23 @@ public class MoonWalker extends JPanel
     JFrame frame = new JFrame("Moon Walker");
     MoonWalker m = new MoonWalker();
     frame.add(m);
-    frame.setResizable(false);
-    frame.setSize(890, 890);
+    frame.setSize(900, 900);
     frame.setVisible(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    while (true) 
+    while (true)
     {
-      m.move(); 
-      m.repaint(); 
+      m.move();
+      m.repaint();
       Thread.sleep(15);
-      endTime = (System.currentTimeMillis())-startTime;
-    } 
-  }
-  
-  public void unCollect()
-  {
-    c1.unCollect();
-    c2.unCollect();
-    c3.unCollect();
-    b.unPress();
-  }
-  
-  public void checkBlock()
-  {
-    for(int i = 0; i < p.length; i++)
-    {
-      sam.checkBlock(p[i]);
     }
   }
-  
-  public void pauseTimer()
+  public void paint(Graphics g)
   {
-    startTime += 15;
-  }
-  
-  public void resetTimer()
-  {
-    startTime = System.currentTimeMillis();
-  }
-  
-  public void paint(Graphics g) 
-  { 
     super.paint(g);
     Graphics2D g2d = (Graphics2D) g;
     g2d.setColor(new Color(0, 0, 100));
     g2d.fillRect(0, 0, 900, 865);
     g2d.setColor(new Color(0, 255, 255));
-    time = "" + (endTime/1000);
-    g2d.setFont(new Font("Aharoni", Font.BOLD, 24));
-    g2d.drawString(time, 500, 50);
     sam.paint(g2d);
     for(int i = 0; i < p.length; i++)
     {
@@ -161,24 +139,16 @@ public class MoonWalker extends JPanel
     c1.paint(g2d);
     c2.paint(g2d);
     c3.paint(g2d);
-    b.paint(g2d);
   }
-  
   public void move()
   {
     for(int i = 0; i < p.length; i++)
     {
       sam.collision(p[i]);
     }
-    //If pressed, count down
-    if(b.getPressed())
-    {
-      b.timeDown();
-    }
     sam.collision(c1);
     sam.collision(c2);
     sam.collision(c3);
-    sam.collision(b);
     sam.move();
   }
 }
